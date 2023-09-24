03b2decd2f1f3002fa43c93fe539608b36289858
Michael Aldana <ms.mstatoo@gmail.com>
2023-09-24 01:30:04 -0400

    added round function and total score logic

--- java.js
+++ java.js
@@ -2,7 +2,9 @@ let result;
 let i;
 let h;
 let validChoices = ["rock", "paper", "scissors"];
-
+let j;
+let w = 0;
+let l = 0;
 // get a random string from array
 
 function getRandom(stringArray) {
@@ -10,8 +12,12 @@ function getRandom(stringArray) {
     return stringArray[randomIndex];
 }
 
-// 
-
+// round function
+function round(){
+    ask();
+    getComputerChoice();
+    calc();
+}
 function getComputerChoice() {
     h = getRandom(validChoices);
 }
@@ -25,21 +31,38 @@ function calc(){
     }
     else if (i === "rock" && h === "paper" || i === "paper" && h === "scissors" || i === "scissors" && h === "rock"){
         console.log("LOST Player:" + i + " JARVIS:" + h)
+        l++
     }
     else {
         console.log("WON Player:" + i + " JARVIS:" + h)
+        w++
     }
 }
 
 // ask user for correct play
 
-do {
-    i = prompt("Welcome to Rock, Paper, and Scissors. Please enter your play (rock, paper, or scissors): ");
-    result = i.toLowerCase();
-} while (!validChoices.includes(result));
+function ask(){
+    do {
+        i = prompt("Welcome to Rock, Paper, and Scissors. Please enter your play (rock, paper, or scissors): ");
+        result = i.toLowerCase();
+    } while (!validChoices.includes(result));
+}
 
-getComputerChoice();
-calc();
+for(j = 0; j < 5; j++)
+{
+    console.log("round " + (j + 1));
+    round();
+}
+if (l > w){
+    console.log("You Lost the game");
+}
+else if (w > l){
+    console.log("You Won the game")
+}
+else {
+    console.log("TIE")
+}
+
 
 
 
